2223fcf8afac2186dd2d2bdf3fc9147b496ed876
Nchristiano <141366844+Nchristiano@users.noreply.github.com>
2024-10-07 11:02:03 -0300

    jhhu

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 
 criaCartao(
-    'Programação',
+    'Portal Wheeling',
     'O que é Portal Wheeling ?',
-    'é uma fabrica e loja online responsavel pela criação do quadro de bike PERIFA '
+    'é uma fabrica e loja online responsavel pela criação de peças e componentes debicicletas e também responsavel pela criacao do quadro de PERIFA, entre outros '
 )
 
 criaCartao(
-    'Geografia',
-    'quemé o criador da marca ?',
+    'criador',
+    'quem é o criador da marca ?',
     'o criador e mentor da empresa se chama Hudson Amorim'
 )
 
 criaCartao(
-    'Programação',
+    'proposito da empresa',
     'qual o proposito da empresa?',
     ' produzir peças e componestes de alta qualidade para uma bicicleta, comercializar variados tipos de bicicletas, casuais, esportivas, e biciclestas para manobras '
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'sobre a empresa',
     'quando foi criada tal empresa?',
     'O Portal Wheeling nasceu em 2013'
 )
